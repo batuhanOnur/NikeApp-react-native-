23553b5775feb88c9392caf738445cd925c173b7
batuhanOnur <49373188+batuhanOnur@users.noreply.github.com>
2023-02-11 00:07:36 +0300

    creating a flat list

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import { StyleSheet, Text, View,Image,FlatList } from 'react-native';
+import products from './src/data/products'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{}}/>
-
+      <FlatList 
+        data={products}
+        numColumns={2}
+        renderItem={({ item })=> (
+          <View style={styles.itemContainer}>
+            <Image 
+              source={{ uri: item.image}}
+              style={styles.image}
+            />
+          </View>
+        )}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +29,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width: '100%',
+    aspectRatio: 1
+  },
+  itemContainer:{
+    width: '50%',
+    padding: 1,
+  }
 });
