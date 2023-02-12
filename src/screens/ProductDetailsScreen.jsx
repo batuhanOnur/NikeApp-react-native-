15430520a48d26c226eaa5948769f8a2633632b9
batuhanOnur <49373188+batuhanOnur@users.noreply.github.com>
2023-02-12 12:29:29 +0300

    addtocart button

--- src/screens/ProductDetailsScreen.jsx
+++ src/screens/ProductDetailsScreen.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { StyleSheet, Text, View,Image, FlatList, useWindowDimensions, ScrollView } from 'react-native'
+import { StyleSheet, Text, View,Image, FlatList, useWindowDimensions, ScrollView,Pressable } from 'react-native'
 import products from '../data/products'
 
 
 const ProductDetailsScreen = () => {
     
     const product = products[0];
-
+    
     const { width } = useWindowDimensions()
+    
+    const addToCart = () => {
+        console.warn("add to cart")
+    }
     
     return(
         <View>
@@ -37,8 +41,11 @@ const ProductDetailsScreen = () => {
                 <Text style={styles.description}>{ product.description }</Text>
             </View>
         </ScrollView>
-
+        
         {/* Add Cart Button */}
+        <Pressable onPress={addToCart} style={styles.button}>
+            <Text style={styles.buttonText}>Add to Cart</Text>
+        </Pressable>
         </View>
     )
 }
@@ -61,5 +68,20 @@ const styles = StyleSheet.create({
         fontSize: 18,
         lineHeight: 30,
         fontWeight: '300',
+    },
+    button:{
+        position: 'absolute',
+        backgroundColor: 'black',
+        bottom: 30,
+        width: '90%',
+        alignSelf: 'center',
+        padding: 20,
+        borderRadius: 100,
+        alignItems: 'center'
+    },
+    buttonText:{
+        color: 'white',
+        fontWeight: '500',
+        fontSize: 16
     }
 })
