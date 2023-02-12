053be1b8d864571daa50f784031ddab778d3c706
batuhanOnur <49373188+batuhanOnur@users.noreply.github.com>
2023-02-12 19:20:25 +0300

    shopping cart design

---
+++ src/screens/ShoppingCart.jsx
@@ -0,0 +1,77 @@
+import { StyleSheet, Text, View,FlatList,Pressable } from 'react-native'
+import cart from '../data/cart'
+import CartListItem from '../components/CartListItem';
+
+const ShoppingCart = () => {
+    return (
+        <>
+        <FlatList 
+            data={cart}
+            renderItem={({ item }) => (
+                <CartListItem cartItem={item}/>
+            )}
+            ListFooterComponent={()=> (
+                <View style={styles.totalsContainer}>
+                    <View style={styles.row}>
+                        <Text style={styles.text}>Subtotal</Text>
+                        <Text style={styles.text}>410.00 $</Text>
+                    </View>
+
+                    <View style={styles.row}>
+                        <Text style={styles.text}>Delivery</Text>
+                        <Text style={styles.text}>10.00 $</Text>
+                    </View>
+
+                    <View style={styles.row}>
+                        <Text style={styles.textBold}>Total</Text>
+                        <Text style={styles.textBold}>420.00 $</Text>
+                    </View>
+                </View>
+            )}
+        />
+        
+        <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Checkout</Text>
+        </Pressable>
+        </>
+    )
+}
+
+export default ShoppingCart
+
+const styles = StyleSheet.create({
+    totalsContainer:{
+        margin: 20,
+        paddingTop: 10,
+        borderColor: 'gainsboro',
+        borderTopWidth: 1
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: 2
+    },
+    text:{
+        fontSize: 16,
+        color: 'gray'
+    },
+    textBold:{
+        fontSize: 16,
+        fontWeight: '500'
+    },
+    button:{
+        position: 'absolute',
+        backgroundColor: 'black',
+        bottom: 30,
+        width: '90%',
+        alignSelf: 'center',
+        padding: 20,
+        borderRadius: 100,
+        alignItems: 'center'
+    },
+    buttonText:{
+        color: 'white',
+        fontWeight: '500',
+        fontSize: 16
+    }
+})
